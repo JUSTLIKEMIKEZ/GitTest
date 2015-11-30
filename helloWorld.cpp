18a5ab0baac75ae3eb30d9dd828fec87cabce417
Michael Handria <michaelhandria@gmail.com>
2015-11-30 01:36:07 -0800

    added a new file

---
+++ helloWorld.cpp
@@ -0,0 +1,18 @@
+//////////////////////////////////////////
+//
+//	this is just a test for github
+//	nothing serious just a "hello world"
+//	program. 
+//
+/////////////////////////////////////////
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void){
+
+	cout << "Hello World";
+	return 0;
+}
